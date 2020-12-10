63d67ca4149cc78efccaa8360c94fe295cc08adb
sebaav12 <oavendano@uc.cl>
2020-12-09 23:56:13 -0300

    temporizador

--- lib/pages/temporizador_page.dart
+++ lib/pages/temporizador_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class TemporizadorPage extends StatefulWidget {
@@ -9,170 +10,217 @@ class TemporizadorPage extends StatefulWidget {
 }
 
 class _TemporizadorPageState extends State<TemporizadorPage> {
-  double percent = 0;
-  static int TimeInMinut = 25;
-  int TimeInSec = TimeInMinut * 60;
+  int hour = 0;
+  int min = 0;
+  int sec = 0;
 
-  Timer timer;
+  bool started = true;
+  bool stopped = true;
 
-  _StartTimer() {
-    TimeInMinut = 25;
-    int Time = TimeInMinut * 60;
-    double SecPercent = (Time / 100);
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+  int timeForTimer = 0;
+
+  String timetodisplay = "";
+
+  bool checktimer = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _timer(context),
+      ),
+    );
+  }
+
+  void start() {
+    setState(() {
+      started = false;
+      stopped = false;
+    });
+    timeForTimer = ((hour * 60 * 60) + (min * 60) + sec);
+    Timer.periodic(
+        Duration(
+          seconds: 1,
+        ), (Timer t) {
       setState(() {
-        if (Time > 0) {
-          Time--;
-          if (Time % 60 == 0) {
-            TimeInMinut--;
-          }
-          if (Time % SecPercent == 0) {
-            if (percent < 1) {
-              percent += 0.01;
-            } else {
-              percent = 1;
-            }
-          }
+        if (timeForTimer < 1 || checktimer == false) {
+          t.cancel();
+        } else if (timeForTimer < 60) {
+          timetodisplay = timeForTimer.toString();
+          timeForTimer = timeForTimer - 1;
+        } else if (timeForTimer < 3600) {
+          int m = timeForTimer ~/ 60;
+          int s = timeForTimer - (60 * m);
+          timetodisplay = m.toString() + ":" + s.toString();
+          timeForTimer = timeForTimer - 1;
         } else {
-          percent = 0;
-          TimeInMinut = 25;
-          timer.cancel();
+          int h = timeForTimer ~/ 3600;
+          int t = timeForTimer - (3600 * h);
+          int m = t ~/ 60;
+          int s = t - (60 * m);
+          timetodisplay =
+              h.toString() + ":" + m.toString() + ":" + s.toString();
+          timeForTimer = timeForTimer - 1;
         }
       });
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Color(0xff1542bf), Color(0xff51a8ff)],
-                  begin: FractionalOffset(0.5, 1))),
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Text(
-                  "Temporizador",
-                  style: TextStyle(color: Colors.white, fontSize: 30.0),
-                ),
-              ),
-              Expanded(
-                child: CircularPercentIndicator(
-                  circularStrokeCap: CircularStrokeCap.round,
-                  percent: percent,
-                  animation: true,
-                  animateFromLastPercent: true,
-                  radius: 200.0,
-                  lineWidth: 20.0,
-                  progressColor: Colors.white,
-                  center: Text(
-                    "$TimeInMinut",
-                    style: TextStyle(color: Colors.white, fontSize: 80.0),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 0.0,
-              ),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(15.0),
-                          topLeft: Radius.circular(15.0))),
-                  child: Padding(
-                    padding:
-                        EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: Column(
-                                  children: <Widget>[
-                                    Text(
-                                      "Tiempo",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 15.0,
-                                    ),
-                                    Text(
-                                      "25",
-                                      style: TextStyle(
-                                        fontSize: 50.0,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: <Widget>[
-                                    Text(
-                                      "Tiempo",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 15.0,
-                                    ),
-                                    Text(
-                                      "5",
-                                      style: TextStyle(
-                                        fontSize: 50.0,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+  void stop() {
+    setState(() {
+      started = true;
+      stopped = true;
+      checktimer = false;
+    });
+  }
+
+  Widget _timer(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Text(
+                        "HH",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
                         ),
-                        Padding(
-                            padding: EdgeInsets.symmetric(vertical: 28.0),
-                            child: RaisedButton(
-                              onPressed: _StartTimer,
-                              color: Colors.blue,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(100.0),
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  "Start",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 22.0,
-                                  ),
-                                ),
-                              ),
-                            )),
-                      ],
+                      ),
+                    ),
+                    NumberPicker.integer(
+                      initialValue: hour,
+                      minValue: 0,
+                      maxValue: 23,
+                      listViewWidth: 60.0,
+                      onChanged: (val) {
+                        setState(() {
+                          hour = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Text(
+                        "MM",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    NumberPicker.integer(
+                      initialValue: min,
+                      minValue: 0,
+                      maxValue: 23,
+                      listViewWidth: 60.0,
+                      onChanged: (val) {
+                        setState(() {
+                          min = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Text(
+                        "SS",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    NumberPicker.integer(
+                      initialValue: sec,
+                      minValue: 0,
+                      maxValue: 23,
+                      listViewWidth: 60.0,
+                      onChanged: (val) {
+                        setState(() {
+                          sec = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(
+              timetodisplay,
+              style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: started ? start : null,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 10.0,
+                  ),
+                  color: Colors.green,
+                  child: Text(
+                    "Start",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
                     ),
                   ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
+                RaisedButton(
+                  onPressed: stopped ? null : stop,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 10.0,
+                  ),
+                  color: Colors.red,
+                  child: Text(
+                    "Stop",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
